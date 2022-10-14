d56a0d25042f6aadedec8fa31b9dad6621a766a7
Michoaddy <michoaddy@gmail.com>
2022-10-13 20:57:14 -0700

    its ok now

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
 
-
-
 /**
+ * main- print to screen
  *
- * * main - Print to screen
- *
- * *
- *
- * * Return: Always 0 (Successfully coded)
- *
- * */
-
+ * Return:Always 0 (successfully coded)
+ */
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual
-
-			puzzle");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }

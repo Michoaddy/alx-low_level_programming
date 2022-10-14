70edf6e7a97633b5fe7072ace2bf4e2be34c8c0f
Michoaddy <michoaddy@gmail.com>
2022-10-13 20:23:02 -0700

    ok

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ * * main - Print to screen
+ *
+ * *
+ *
+ * * Return: Always 0 (Successfully coded)
+ *
+ * */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual
+
+			puzzle");
+
+	return (0);
+
+}

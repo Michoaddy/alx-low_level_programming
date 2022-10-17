ae14cc6d7546449b5187019224c0a64268a0f763
Michoaddy <michoaddy@gmail.com>
2022-10-17 14:36:46 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main-program entry point.
+ *Return:0 whenever code run successfully.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}

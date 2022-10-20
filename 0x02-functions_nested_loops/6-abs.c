87ba0237c931e23201ce9409fd3e8f0d8ec9e9f7
Michoaddy <michoaddy@gmail.com>
2022-10-19 20:00:10 -0700

    tast 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - compute the abs value of an integer
+ * @n: the value in question
+ * Return: the abs of the integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
